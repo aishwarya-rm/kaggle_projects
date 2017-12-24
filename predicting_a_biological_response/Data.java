374a331940d9763168ee59da9f365e9e18cace6f
Aishwarya Mandyam <aishwarya_m@live.com>
2017-12-24 01:00:54 -0800

    added notes

--- predicting_a_biological_response/Data.java
+++ predicting_a_biological_response/Data.java
@@ -9,9 +9,20 @@ class Data{
 		String line = null;
 		Scanner scanner = null;
 		int index = 0;
+		int id = 1;
 		ArrayList<String> on = new ArrayList<String();
 		while((line = reader.readLine()) != null){
-			String 
+			Record r = new Record();
+			scanner = new Scanner(line);
+			scanner.useDelimiter(",");
+			while(scanner.hasNext()){
+				String data = scanner.next();
+				if(index == 0){
+					r.setId(id);
+					r.setOn(Integer.parseInt(data) == 1? true: false);
+				}
+				index ++;
+			}
 		}
 	}
 }
---
+++ predicting_a_biological_response/Record.java
@@ -0,0 +1,16 @@
+public class Record{
+	private boolean on;
+	private int id;
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		this.id = id;
+	}
+	public void setOn(boolean on){
+		this.on = on;
+	}
+	public boolean getOn(){
+		return this.on;
+	}
+}
