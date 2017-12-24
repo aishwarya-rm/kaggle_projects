80d127dc274d6e441d9cabd7c3939f858d05d072
Aishwarya Mandyam <aishwarya_m@live.com>
2017-12-23 23:28:01 -0800

    starting

---
+++ predicting_a_biological_response/Data.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+class Data{
+	public static void main(String[] args){
+
+	}
+	public static void readFile(){
+		BufferedReader reader = new BufferedReader(new FileReader('train.csv'));
+		String line = null;
+		Scanner scanner = null;
+		int index = 0;
+		ArrayList<String> on = new ArrayList<String();
+		while((line = reader.readLine()) != null){
+			String 
+		}
+	}
+}
